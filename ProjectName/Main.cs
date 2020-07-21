e93b91f0e77a13d47130df3d558fd0921eaf8329
Hannah <hannah.beinstein@gmail.com>
2020-07-21 10:54:35 -0700

    Initial commit

---
+++ ProjectName/Main.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using ItemNamespace;
+
+namespace ProjectName
+{
+  public class ProjectName
+  {
+    public static void Main()
+    {
+      // ui logic here
+    }
+  }
+}
