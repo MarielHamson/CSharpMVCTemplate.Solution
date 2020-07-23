221b67f4cf2ac060447095e731e20ee0122b9e02
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-22 21:35:03 -0700

    update to use static content

--- ProjectName/Controllers/HomeController.cs
+++ ProjectName/Controllers/HomeController.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using ProjectName.Models;
 using System.Collections.Generic;
 
-using Microsoft.AspNetCore.Mvc;
-
 namespace ProjectName.Controllers
 {
   public class HomeController : Controller
